b011de599457d2c11181d4616b0fa19387865c02
Gabriel Rockson <darkbotbbl@gmail.com>
2021-03-04 06:04:26 +0000

    Added inline admin models in the solutions admin module

--- solutions/admin.py
+++ solutions/admin.py
@@ -1,15 +1,32 @@
 from django.contrib import admin
 
-from .models import StudentSolution, RecommendedSolution
+from .models import StudentSolution, RecommendedSolution, File, Image
 
+
+class ImageInline(admin.StackedInline):
+        model = Image
+        extra = 1
+        max_num = 10
+
+
+class FileInline(admin.StackedInline):
+        model = File
+        extra = 1
+        max_num = 10
 
 class StudentSolutionAdmin(admin.ModelAdmin):
 	list_display = ["title"]
-
+	inlines = [
+        ImageInline,
+        FileInline,
+    ]
 
 class RecommendedSolutionAdmin(admin.ModelAdmin):
 	list_display = ["title"]
-
+	inlines = [
+        ImageInline,
+        FileInline,
+    ]
 
 admin.site.register(StudentSolution, StudentSolutionAdmin)
 admin.site.register(RecommendedSolution, RecommendedSolutionAdmin)
