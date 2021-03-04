e2c6f4d38b5e6a30003d6bfc35c8f2b0ed7263a2
Gabriel Rockson <darkbotbbl@gmail.com>
2021-03-04 05:53:50 +0000

    Added extra models for the image and files so as to to able to add as many as possible to each problem

--- problems/admin.py
+++ problems/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
-from .models import Problem
+from .models import Problem, Image, File
 from solutions.models import StudentSolution
 
 
+class ImageInline(admin.StackedInline):
+        model = Image
+        extra = 1
+
+
+class FileInline(admin.StackedInline):
+        model = File
+        extra = 1
+
 class ProblemAdmin(admin.ModelAdmin):
-	list_display = ["title", "created"]
+    list_display = ["title", "created"]
+    inlines = [
+        ImageInline,
+        FileInline,
+    ]
 
 
 admin.site.register(Problem, ProblemAdmin)
--- problems/models.py
+++ problems/models.py
@@ -4,90 +4,89 @@ from django.urls import reverse
 
 
 class Problem(models.Model):
-	EASY = "E"
-	MEDIUM = "M"
-	HARD = "H"
-	EXTREME = "Em"
-	DIFFICULTY_CHOICES = (
-		(EASY, "Easy"),
-		(MEDIUM, "Medium"),
-		(HARD, "Hard"),
-		(EXTREME, "Extreme"),
-	)
-	PROGRAMMING = "P"
-	CIRCUIT_DESIGN = "D"
-	WEB_DEVELOPMENT = "W"
-	SYSTEMS_DESIGN = "S"
-	PROBLEM_CATEGORY = (
-		(PROGRAMMING, "Programming"),
-		(CIRCUIT_DESIGN, "Electrical Circuit Design"),
-		(WEB_DEVELOPMENT, "Web Development"),
-		(SYSTEMS_DESIGN, "Electrical Systems Design"),
-	)
+    EASY = "E"
+    MEDIUM = "M"
+    HARD = "H"
+    EXTREME = "Em"
+    DIFFICULTY_CHOICES = (
+        (EASY, "Easy"),
+        (MEDIUM, "Medium"),
+        (HARD, "Hard"),
+        (EXTREME, "Extreme"),
+    )
+    PROGRAMMING = "P"
+    CIRCUIT_DESIGN = "D"
+    WEB_DEVELOPMENT = "W"
+    SYSTEMS_DESIGN = "S"
+    PROBLEM_CATEGORY = (
+        (PROGRAMMING, "Programming"),
+        (CIRCUIT_DESIGN, "Electrical Circuit Design"),
+        (WEB_DEVELOPMENT, "Web Development"),
+        (SYSTEMS_DESIGN, "Electrical Systems Design"),
+    )
 
-	title = models.CharField(
-		max_length=500, 
-		null=False, 
-		blank=False, 
-		help_text="Enter the problem statement"
-	)
-	slug = AutoSlugField(
-		populate_from="title",
-		unique=True
-	)
-	created = models.DateTimeField(auto_now_add=True)
-	deadline = models.DateTimeField()
-	image_one = models.ImageField(
-		upload_to="problems/images",
-		null=True,
-		blank=True
-	)
-	image_two = models.ImageField(
-		upload_to="problems/images",
-		null=True,
-		blank=True
-	)
-	image_three = models.ImageField(
-		upload_to="problems/images",
-		null=True,
-		blank=True
-	)
-	# todo - the problem description field needs to be a rich text field
-	# todo - you need to add a field so admins can add a code snippet, should the problem require it.
-	problem_description = models.TextField()
-	difficulty = models.CharField(
-		choices=DIFFICULTY_CHOICES,
-		default=MEDIUM,
-		max_length=50
-	)
-	points_worth = models.PositiveIntegerField(default=5)
-	category = models.CharField(
-		choices=PROBLEM_CATEGORY,
-		default=PROGRAMMING,
-		max_length=50
-	)
-	file_one = models.FileField(
-		upload_to="problems/files",
-		null=True,
-		blank=True,
-	)
-	file_two = models.FileField(
-		upload_to="problems/files",
-		null=True,
-		blank=True,
-	)
-	file_three = models.FileField(
-		upload_to="problems/files",
-		null=True,
-		blank=True,
-	)
+    title = models.CharField(
+        max_length=500, 
+        null=False, 
+        blank=False, 
+        help_text="Enter the problem statement"
+    )
+    slug = AutoSlugField(
+        populate_from="title",
+        unique=True
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    # todo - the problem description field needs to be a rich text field
+    # todo - you need to add a field so admins can add a code snippet, should the problem require it.
+    problem_description = models.TextField()
+    difficulty = models.CharField(
+        choices=DIFFICULTY_CHOICES,
+        default=MEDIUM,
+        max_length=50
+    )
+    points_worth = models.PositiveIntegerField(default=5)
+    category = models.CharField(
+        choices=PROBLEM_CATEGORY,
+        default=PROGRAMMING,
+        max_length=50
+    )
 
-	class Meta:
-		ordering = ["-created"]
+    class Meta:
+        ordering = ["-created"]
 
-	def __str__(self):
-		return self.title
-	
-	def get_absolute_url(self):
-    		return reverse("problems:problem-detail", kwargs={"slug": self.slug})
-	
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+            return reverse("problems:problem-detail", kwargs={"slug": self.slug})
+    
+
+class File(models.Model):
+    problem = models.ForeignKey(
+        Problem,
+        related_name="files",
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255, help_text="Enter a name to identify this file")
+    file = models.FileField(
+        upload_to="problems/files/"
+    )
+      
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    problem = models.ForeignKey(
+        Problem,
+        related_name="images",
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255, help_text="Enter a name to identify this image")
+    image = models.ImageField(
+        upload_to="problems/images"
+    )
+
+    def __str__(self):
+        return self.title
